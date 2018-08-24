488341650687dab4434615f537a52c00818befa3
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-08-24 14:18:09 +0200

    numb event handling

--- index.js
+++ index.js
@@ -7,31 +7,32 @@ function HashtagStreamSet (willDelete) {
   if (!(this instanceof HashtagStreamSet))
     return new HashtagStreamSet(willDelete)
   StreamSet.call(this)
-  this.on('remove', stream => this._delete(stream._hashtag, willDelete))
+  this.on('remove', this._onremove.bind(this, willDelete))
 }
 
 inherits(HashtagStreamSet, StreamSet)
 
+HashtagStreamSet.prototype._onremove = function (willDelete, stream) {
+  this._delete(stream._hashtag, willDelete)
+}
+
+HashtagStreamSet.prototype._doDelete = function (tag) {
+  var rm_stream
+  while ((rm_stream = this.streams.find(stream => stream._hashtag === tag))) {
+    rm_stream._hashtag = undefined
+    this.off('remove', this._onremove)
+    this.remove(rm_stream)
+    this.on('remove', this._onremove)
+  }
+}
+
 HashtagStreamSet.prototype._delete = function (tag, willDelete) {
   if (typeof willDelete !== 'function')
     willDelete = (tag, streams, doDelete) => doDelete()
-
-  const doDelete = () => {
-    var rm_stream
-    while ((rm_stream = this.streams.find(stream => stream._hashtag === tag))) {
-      rm_stream._hashtag = undefined
-      this.remove(rm_stream)
-    }
-  }
-
   const rm_streams = this.streams.filter(stream => stream._hashtag === tag)
-
-  if (rm_streams.length) {
-    willDelete(tag, rm_streams, doDelete)
-    return true
-  } else {
-    return false
-  }
+  if (!rm_streams.length) return false
+  willDelete(tag, rm_streams, this._doDelete.bind(this, tag))
+  return true
 }
 
 HashtagStreamSet.prototype.add = function (tag, ...streams) {
@@ -42,6 +43,7 @@ HashtagStreamSet.prototype.add = function (tag, ...streams) {
       return stream
     })
     .forEach(StreamSet.prototype.add.bind(this))
+    return true
 }
 
 HashtagStreamSet.prototype.delete = function (tag, willDelete) {
